99a248923758a14a1660070b7b47ef000b2ab2fe
Alexander Hwang <hwangalexander7@gmail.com>
2017-04-25 15:28:09 -0400

    Add files via upload
    Delete button and more... First attemp

---
+++ edit.php
@@ -0,0 +1,166 @@
+<html>
+
+<head>
+  <style>
+  html {
+    font-family: GillSans, sans-serif;
+    text-align: center;
+  }
+  </style>
+
+    <script
+    src="https://code.jquery.com/jquery-3.2.1.min.js"
+    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+    crossorigin="anonymous">
+  </script>
+
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+
+
+
+</head>
+
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo center">Logo</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  
+      <div class="row">
+        <div class="col s12 m6">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">Card Title</span>
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+        
+    
+	<?php
+	
+	    // pass in some info;
+		require("common.php"); 
+		
+		if(empty($_SESSION['user'])) { 
+  
+			// If they are not, we redirect them to the login page. 
+			$location = "http://" . $_SERVER['HTTP_HOST'] . "/login.php";
+			echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
+			//exit;
+         
+        	// Remember that this die statement is absolutely critical.  Without it, 
+        	// people can view your members-only content without logging in. 
+        	die("Redirecting to login.php"); 
+    	} 
+		
+		// To access $_SESSION['user'] values put in an array, show user his username
+		$arr = array_values($_SESSION['user']);
+
+
+		// open connection
+		$connection = mysqli_connect($host, $username, $password) or die ("Unable to connect!");
+
+		// select database
+		mysqli_select_db($connection, $dbname) or die ("Unable to select database!");
+
+		// create query
+		$query = "SELECT * FROM symbols";
+       
+		// execute query
+		$result = mysqli_query($connection,$query) or die ("Error in query: $query. ".mysql_error());
+
+		// see if any rows were returned
+		if (mysqli_num_rows($result) > 0) {
+
+    		// print them one after another
+
+    		echo "<table class='centered' >";
+    		while($row = mysqli_fetch_row($result)) {
+        		echo "<tr>";
+				echo "<td>".$row[0]."</td>";
+        		echo "<td>" . $row[1]."</td>";
+        		echo "<td>".$row[2]."</td>";
+				echo "<td><a class= 'waves-effect waves-light btn' href=".$_SERVER['PHP_SELF']."?id=".$row[0].">Delete</a></td>";
+        		echo "</tr>";
+    		}
+		    echo "</table>";
+
+
+
+
+		} else {
+			
+    		// print status message
+    		echo "No rows found!";
+		}
+
+		// free result set memory
+		mysqli_free_result($connection,$result);
+
+		// set variable values to HTML form inputs
+
+		$country = $_POST['country'];
+    	$animal = $_POST['animal'];
+		
+		// check to see if user has entered anything
+		if ($animal != "") {
+	 		// build SQL query
+			$query = "INSERT INTO symbols (country, animal) VALUES ('$country', '$animal')";
+			// run the query
+     		$result = mysqli_query($connection,$query) or die ("Error in query: $query. ".mysql_error());
+			// refresh the page to show new update
+	 		echo "<meta http-equiv='refresh' content='0'>";
+		}
+		
+		// if DELETE pressed, set an id, if id is set then delete it from DB
+		if (isset($_GET['id'])) {
+
+			// create query to delete record
+			echo $_SERVER['PHP_SELF'];
+    		$query = "DELETE FROM symbols WHERE id = ".$_GET['id'];
+
+			// run the query
+     		$result = mysqli_query($connection,$query) or die ("Error in query: $query. ".mysql_error());
+			
+			// reset the url to remove id $_GET variable
+			$location = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+			echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
+			exit;
+			
+		}
+
+
+		
+		// close connection
+		mysqli_close($connection);
+
+	?>
+    
+    <!-- This is the HTML form that appears in the browser -->
+
+   	<form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+    	Country: <input type="text" name="country">
+    	National animal: <input type="text" name="animal">
+    	<input type="submit" name="submit">
+    </form>
+    <form action="logout.php" method="post"><button>Log out</button></form>
+    
+	</body>
+</html>
