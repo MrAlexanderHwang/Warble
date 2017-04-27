2ce1dac4ff159801e9d2db942933347f3c6b7394
e-vandenberg <erik.vandenberg@ucc.on.ca>
2017-04-27 09:37:55 -0400

    Update edit.php
    Added search (doesn't yet search stuff)

--- edit.php
+++ edit.php
@@ -30,14 +30,22 @@
       <div class="nav-wrapper">
         <a href="#" class="brand-logo center">Warble</a>
         <ul id="nav-mobile" class="left hide-on-med-and-down">
-          <li><a href = "#!?"> <i class="material-icons">search</i></a></li>
           <li><a href="badges.html">My Account</a></li>
           <li><a href="badges.html">Help</a></li>
           <li><a href="badges.html">About</a></li>
-
+          <li>
+            <form>
+              <div class="input-field">
+                <input id="search" type="search" required>
+                <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                <i class="material-icons">close</i>
+              </div>
+            </form>
+          </li>
         </ul>
       </div>
     </nav>
+
 
     <div class="container">
 
