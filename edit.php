d2cdbb09d07437999e66ea2d3dbab89e3a1e8878
e-vandenberg <erik.vandenberg@ucc.on.ca>
2017-04-27 08:33:24 -0400

    Update edit.php
    Side-nav

--- edit.php
+++ edit.php
@@ -1,20 +1,12 @@
 <html>
-
-<head>
-  <style>
-  html {
-    font-family: GillSans, sans-serif;
-    text-align: center;
-  }
-  </style>
-
+  <head>
     <script
-    src="https://code.jquery.com/jquery-3.2.1.min.js"
-    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-    crossorigin="anonymous">
-  </script>
+      src="https://code.jquery.com/jquery-3.2.1.min.js"
+      integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+      crossorigin="anonymous">
 
-  <!-- Compiled and minified CSS -->
+    </script>
+    <!-- Compiled and minified CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
 
   <!-- Compiled and minified JavaScript -->
@@ -22,57 +14,47 @@
 
 
 
-</head>
+  <script>
+  $(document).ready(function(){
+    $('.modal').modal)();
+  })
+  </script>
 
-  <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo center">Logo</a>
-      <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
+  </head>
 
-  
-      <div class="row">
-        <div class="col s12 m6">
-          <div class="card blue-grey darken-1">
-            <div class="card-content white-text">
-              <span class="card-title">Card Title</span>
-              <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
-            </div>
-            <div class="card-action">
-              <a href="#">This is a link</a>
-              <a href="#">This is a link</a>
-            </div>
-          </div>
-        </div>
+	<body>
+
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo center">Warble</a>
+        <ul id="nav-mobile" class="left hide-on-med-and-down">
+          <li><a href="sass.html">Search</a></li>
+          <li><a href="badges.html">My Account</a></li>
+          <li><a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">CC</i></a></li>
+        </ul>
       </div>
-        
-    
+    </nav>
+
+    <div class="container">
 	<?php
-	
+
 	    // pass in some info;
-		require("common.php"); 
-		
-		if(empty($_SESSION['user'])) { 
-  
-			// If they are not, we redirect them to the login page. 
+		require("common.php");
+
+		if(empty($_SESSION['user'])) {
+
+			// If they are not, we redirect them to the login page.
 			$location = "http://" . $_SERVER['HTTP_HOST'] . "/login.php";
 			echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
 			//exit;
-         
-        	// Remember that this die statement is absolutely critical.  Without it, 
-        	// people can view your members-only content without logging in. 
-        	die("Redirecting to login.php"); 
-    	} 
-		
+
+        	// Remember that this die statement is absolutely critical.  Without it,
+        	// people can view your members-only content without logging in.
+        	die("Redirecting to login.php");
+    	}
+
 		// To access $_SESSION['user'] values put in an array, show user his username
 		$arr = array_values($_SESSION['user']);
-
 
 		// open connection
 		$connection = mysqli_connect($host, $username, $password) or die ("Unable to connect!");
@@ -82,7 +64,7 @@
 
 		// create query
 		$query = "SELECT * FROM symbols";
-       
+
 		// execute query
 		$result = mysqli_query($connection,$query) or die ("Error in query: $query. ".mysql_error());
 
@@ -90,35 +72,32 @@
 		if (mysqli_num_rows($result) > 0) {
 
     		// print them one after another
-
-    		echo "<table class='centered' >";
+    		echo "<table cellpadding=50 border=1>";
     		while($row = mysqli_fetch_row($result)) {
         		echo "<tr>";
 				echo "<td>".$row[0]."</td>";
         		echo "<td>" . $row[1]."</td>";
         		echo "<td>".$row[2]."</td>";
-				echo "<td><a class= 'waves-effect waves-light btn' href=".$_SERVER['PHP_SELF']."?id=".$row[0].">Delete</a></td>";
+				echo "<td><a href=".$_SERVER['PHP_SELF']."?id=".$row[0].">Delete</a></td>";
         		echo "</tr>";
     		}
 		    echo "</table>";
 
-
-
-
 		} else {
-			
+
     		// print status message
-    		echo "No rows found!";
+        echo "<script> Materialize.toast('Looks like there isnt anything new here', 6500); // 6500 is the duration of the toast </script>";
 		}
+
+
 
 		// free result set memory
 		mysqli_free_result($connection,$result);
 
 		// set variable values to HTML form inputs
-
 		$country = $_POST['country'];
     	$animal = $_POST['animal'];
-		
+
 		// check to see if user has entered anything
 		if ($animal != "") {
 	 		// build SQL query
@@ -128,7 +107,7 @@
 			// refresh the page to show new update
 	 		echo "<meta http-equiv='refresh' content='0'>";
 		}
-		
+
 		// if DELETE pressed, set an id, if id is set then delete it from DB
 		if (isset($_GET['id'])) {
 
@@ -138,29 +117,51 @@
 
 			// run the query
      		$result = mysqli_query($connection,$query) or die ("Error in query: $query. ".mysql_error());
-			
+
 			// reset the url to remove id $_GET variable
 			$location = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
 			echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
 			exit;
-			
+
 		}
 
-
-		
 		// close connection
 		mysqli_close($connection);
 
 	?>
-    
-    <!-- This is the HTML form that appears in the browser -->
 
+
+  <ul id="slide-out" class="side-nav">
+    <li><div class="userView">
+      <div class="background">
+        <img src="images/office.jpg">
+      </div>
+      <a href="#!user"><img class="circle" src="images/yuna.jpg"></a>
+      <a href="#!name"><span class="white-text name">John Doe</span></a>
+      <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
+    </div></li>
+    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+    <li><a href="#!">Second Link</a></li>
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">Subheader</a></li>
+    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+  </ul>
+  <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">Categories</i></a>
+
+
+    <!-- This is the HTML form that appears in the browser -->
    	<form action="<?=$_SERVER['PHP_SELF']?>" method="post">
     	Country: <input type="text" name="country">
     	National animal: <input type="text" name="animal">
     	<input type="submit" name="submit">
     </form>
     <form action="logout.php" method="post"><button>Log out</button></form>
-    
+
+  <script>
+  $(function(){
+    $(".button-collapse").sideNav();
+  })
+  </script>
+   </div>
 	</body>
 </html>
